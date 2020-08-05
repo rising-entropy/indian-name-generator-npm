774fd29bf4c9cb0de062784c03fc570cee11594c
Devang Kamble <risingentropy20@gmail.com>
2020-08-05 23:57:15 +0530

    first draft

---
+++ main.js
@@ -0,0 +1,4 @@
+var mainName = ["Gagan", "Har", "Bal", "Man", "Nav", "Sukh", "Kush", "Gur"];
+var maleSuffix = ["jeet", "jyot", "vinder", "preet", "meet"];
+var femaleSuffix = ["preet", "jeet", "bir"];
+
