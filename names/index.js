e4caf5b1320b3f18a1a7b324e3f81eb18e74701a
Anuja2512 <61413633+Anuja2512@users.noreply.github.com>
2020-10-02 20:42:18 +0530

    Updated Malyali Names

--- names/index.js
+++ names/index.js
@@ -3,11 +3,13 @@ var Gujarati = require('./gujarati');
 var Marathi = require('./marathi');
 var Marwari = require('./marwari');
 var Punjabi = require('./punjabi');
+var Malyali = require('./malyali');
 
 module.exports = {
   Bengali,
   Gujarati,
   Marathi,
   Marwari,
-  Punjabi
+  Punjabi,
+  Malyali
 };
---
+++ names/malyali.js
@@ -0,0 +1,13 @@
+var femalemalyali = ["Amla","Alaknanda","Annapurna","Alpa","Amba","Amshula","Amodini","Amrita","Amulya","Ananya","Anandita","Anchita","Anju","Anisha","Anuragini","Anusha","Charulekha","Dhannya","Ecchumati","Indukala","Jayashri","Kamakshi","Kamolika","Kanchana"];
+
+var malemalyali = ["Aadesh","Abimanyu","Akhilesh","Amish","Balaji","Balmohan","Bijoy","Charan","Chetan","Chandu","Chinmay","Chittranjan","Haresh","Harihar","Hemant","Indeevar","Indrajit","Jagdeep","Jayanand","Jayant"];
+
+var unionSuffixMalyali = [...femalemalyali, ...malemalyali];
+
+module.exports = {
+  femalemalyali,
+  malemalyali,
+  unionSuffixMalyali
+};
+
+
