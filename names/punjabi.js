cd393322b36dd4f9ebe9eadeb7b4a333a4f1bcbe
Amrit Atwal <amritatwal04@gmail.com>
2022-02-19 17:32:12 +0000

    Added further Punjabi main names along with both male and female suffixes

--- names/punjabi.js
+++ names/punjabi.js
@@ -1,8 +1,8 @@
-var mainNamePunjabi = ["Gagan", "Har", "Bal", "Man", "Nav", "Sukh", "Kush", "Gur", "Karam", "Karan", "Dil", "Dharam", "Param", "Dal", "Jas", "Par", "Dul"];
+var mainNamePunjabi = ["Gagan", "Har", "Bal", "Man", "Nav", "Sukh", "Kush", "Gur", "Karam", "Karan", "Dil", "Dharam", "Param", "Dal", "Jas", "Par", "Dul", "Aman", "Amrit", "Kul", "Simran", "Raj"];
 
-var maleSuffixPunjabi = ["jeet", "jyot", "vinder", "preet", "meet"];
+var maleSuffixPunjabi = ["jeet", "jyot", "vinder", "preet", "meet", "deep", "pal"];
 
-var femaleSuffixPunjabi = ["preet", "jeet", "bir"];
+var femaleSuffixPunjabi = ["preet", "jeet", "bir", "deep"];
 
 let unionSuffixPunjabi = [...maleSuffixPunjabi, ...femaleSuffixPunjabi];
 
